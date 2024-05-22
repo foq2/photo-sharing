296258e1d5a922cccbf4deeefc46ad179cfe8b4e
foq <hungphongb@gmail.com>
2024-05-22 18:21:50 +0700

    forget sth

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ const app = express();
 const cors = require("cors");
 const dbConnect = require("./db/dbConnect");
 require("dotenv").config();
-
+const Auth = require("./route/auths");
+const Photo = require("./route/photo");
+const User = require("./route/user");
+const Comment = require("./route/comment");
 dbConnect();
 
 app.use(cors());
 app.use("/images", express.static("images"));
 app.use(express.json());
-
+app.use("/api/auth", Auth);
+app.use("/api/photo", Photo);
+app.use("/api/user", User);
+app.use("/api/comment", Comment);
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
